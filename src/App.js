547fda48153368a13b3a78d1f5f7808d8ea6f44d
Jitesh <kanyaljitesh)@gmail.com>
2022-04-23 21:23:53 +0530

    initiat commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App = () => {
+  let pageSize = 5
+  let apiKey = process.env.REACT_APP_API
+  const [progress, setProgress] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={progress}
+      />
+      <Routes>
+        <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+        <Route path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
+        <Route path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+        <Route path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+        <Route path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
+        <Route path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
+        <Route path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+
+}
+export default App;
